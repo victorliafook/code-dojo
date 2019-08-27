a28b123c5bdf438f27840d6c4685be4c3ac525ca
Victor Lia Fook <victorliafook@gmail.com>
2019-08-27 12:26:48 +0000

    making other tests pass

--- bottom-up-fibonacci/fibonacci.js
+++ bottom-up-fibonacci/fibonacci.js
@@ -3,11 +3,21 @@ class fibonacciBU {
         this.sequenceArr = [];
         if (number === 0) {
             this.sequenceArr.push(0);
-        } else if (number === 1) {
+        } else {
+            let sum;
             this.sequenceArr.push(0);
             this.sequenceArr.push(1);
-        } else {
-            
+            for (let i = 2; i <= number; i++) {
+                sum = 0
+                if (this.sequenceArr[i - 2] !== undefined) {
+                    sum += this.sequenceArr[i - 2];
+                }
+                
+                if (this.sequenceArr[i - 1] !== undefined) {
+                    sum += this.sequenceArr[i - 1];
+                }
+                this.sequenceArr.push(sum);
+            }
         }
         
         return this.sequenceArr;
