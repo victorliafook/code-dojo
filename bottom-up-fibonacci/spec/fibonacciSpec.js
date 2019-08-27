2ebcbe5450d32fe7bdcdc5bc481988ef01795046
Victor Lia Fook <victorliafook@gmail.com>
2019-08-27 12:10:40 +0000

    adding tests from other fib directory

---
+++ bottom-up-fibonacci/spec/fibonacciSpec.js
@@ -0,0 +1,27 @@
+const Fibonacci = require('../fibonacci');
+
+describe("Fibonacci Tests", function() {
+    it("Returns valid sequences on base cases", function() {
+        let fib = new Fibonacci();
+        
+        let sequenceArr = fib.getSequence(0); 
+        expect(sequenceArr.join(',')).toBe('0');
+        
+        sequenceArr = fib.getSequence(1); 
+        expect(sequenceArr.join(',')).toBe('0,1');
+    });
+    
+    it("Returns valid sequences other cases", function() {
+        let fib = new Fibonacci();
+        
+        let sequenceArr = fib.getSequence(2); 
+        expect(sequenceArr.join(',')).toBe('0,1,1');
+        
+        sequenceArr = fib.getSequence(3); 
+        expect(sequenceArr.join(',')).toBe('0,1,1,2');
+        
+        sequenceArr = fib.getSequence(6); 
+        expect(sequenceArr.join(',')).toBe('0,1,1,2,3,5,8');
+        
+    });
+});
