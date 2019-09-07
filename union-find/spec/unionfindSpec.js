59f10c84e7a87b7f87738e523c57ead05b81bb0a
Victor Lia Fook <victorliafook@gmail.com>
2019-09-07 03:15:31 +0000

    dealing with an empty set

--- union-find/spec/unionfindSpec.js
+++ union-find/spec/unionfindSpec.js
@@ -3,7 +3,10 @@ const UnionFind = require("../unionfind");
 describe("Union-Find tests", function() {
     it("works for edge cases", function() {
         let numberOfNodes = 0;
-        let UF = new UnionFind(numberOfNodes);
+        let UF;
+        expect(function() {
+            UF = new UnionFind(numberOfNodes);
+        }).toThrowError(/empty set/);
         
         numberOfNodes = 1;
         UF = new UnionFind(numberOfNodes);
--- union-find/unionfind.js
+++ union-find/unionfind.js
@@ -1,3 +1,9 @@
-describe('Union-Find tests', function() {
-    
-});
+class UnionFind {
+    constructor(numberOfNodes) {
+        if (numberOfNodes < 1) {
+            throw new Error("cannot create an empty set");
+        }
+    }
+}
+
+module.exports = UnionFind;
