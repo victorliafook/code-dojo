efc8cc611a2c2cc5d624f749b31230e8afa7be53
Victor Lia Fook <victorliafook@gmail.com>
2019-09-07 03:38:27 +0000

    reestructuring test code

--- union-find/spec/unionfindSpec.js
+++ union-find/spec/unionfindSpec.js
@@ -1,15 +1,17 @@
 const UnionFind = require("../unionfind"); 
 
 describe("Union-Find tests", function() {
-    it("works for edge cases", function() {
+    if("throws exception for an empty set", function() {
         let numberOfNodes = 0;
         let UF;
         expect(function() {
             UF = new UnionFind(numberOfNodes);
         }).toThrowError(/empty set/);
-        
-        numberOfNodes = 1;
-        UF = new UnionFind(numberOfNodes);
+    });
+    
+    it("find operation works for edge cases", function() {
+        let numberOfNodes = 1;
+        let UF = new UnionFind(numberOfNodes);
         
         expect(UF.find(0, 0)).toEqual(true);
         
