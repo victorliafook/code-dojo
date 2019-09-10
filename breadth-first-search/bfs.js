1aa56630d807c1449d9678347c8b5ac4bb52f89a
Victor Lia Fook <victorliafook@gmail.com>
2019-09-10 12:02:08 +0000

    tidying up code

--- breadth-first-search/bfs.js
+++ breadth-first-search/bfs.js
@@ -4,19 +4,18 @@ var bfs = function(graph, startVertex, processVertex) {
     
     queue.push(startVertex);
     
-    while(queue.length > 0) {
+    while (queue.length > 0) {
         let current = queue.shift();
         visited[current] = true;
         processVertex(current);
     
         let adjVertices = graph.adjList[current];
-        for(let vertex in adjVertices) {
-            if(visited[vertex] === undefined) {
+        for (let vertex in adjVertices) {
+            if (visited[vertex] === undefined) {
                 queue.push(vertex);
             }
         }
     }
-
 };
 
 module.exports = bfs;
