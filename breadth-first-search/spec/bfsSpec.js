69e7d66b8c8dd5a91d5494b4daca0a8623f74e54
Victor Lia Fook <victorliafook@gmail.com>
2019-09-10 11:26:51 +0000

    adding extra tests, including cycles

--- breadth-first-search/spec/bfsSpec.js
+++ breadth-first-search/spec/bfsSpec.js
@@ -1,9 +1,9 @@
 const BFS = require("../bfs");
-const DAG = require("../../graph/digraph.js");
+const Digraph = require("../../graph/digraph.js");
 
 describe("BFS tests", function() {
    it("Handles degenerate cases: 1 element", function() {
-      let graph = new DAG(1);
+      let graph = new Digraph(1);
       
       let traversalArr = [];
       let processVertex = function(vertex) {
@@ -16,7 +16,7 @@ describe("BFS tests", function() {
    });
    
    it("Handles degenerate cases: 2 elements, no edge", function() {
-      let graph = new DAG(2);
+      let graph = new Digraph(2);
       
       let traversalArr = [];
       let processVertex = function(vertex) {
@@ -32,7 +32,7 @@ describe("BFS tests", function() {
    });
    
    it("Handles degenerate cases: 2 elements, 1 edge", function() {
-      let graph = new DAG(2);
+      let graph = new Digraph(2);
       graph.addEdge(0, 1);
       
       let traversalArr = [];
@@ -47,4 +47,38 @@ describe("BFS tests", function() {
       BFS(graph, 1, processVertex);
       expect(traversalArr.join(',')).toEqual('1');
    }); 
+   
+   it("Traverses Breadth-First correctly", function() {
+      let graph = new Digraph(4);
+      graph.addEdge(0, 1);
+      graph.addEdge(0, 2); 
+      graph.addEdge(0, 3); 
+      graph.addEdge(2, 4);
+      
+      let traversalArr = [];
+      let processVertex = function(vertex) {
+         traversalArr.push(vertex);
+      };
+      
+      BFS(graph, 1, processVertex);
+      expect(traversalArr.join(',')).toEqual('0,1,2,3,4');
+   });
+   
+   it("Handles cycles", function() {
+      let graph = new Digraph(4);
+      graph.addEdge(0, 1);
+      graph.addEdge(0, 2); 
+      graph.addEdge(1, 2);
+      graph.addEdge(2, 0);
+      graph.addEdge(2, 3);
+      graph.addEdge(3, 3);
+      
+      let traversalArr = [];
+         let processVertex = function(vertex) {
+         traversalArr.push(vertex);
+      };
+      
+      BFS(graph, 2, processVertex);
+      expect(traversalArr.join(',')).toEqual('2,0,3,1');
+   });
 });
