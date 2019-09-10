a8d2af486f90cb344abc7019999b136afc21e9bd
Victor Lia Fook <victorliafook@gmail.com>
2019-09-10 11:01:00 +0000

    making the tests pass

--- breadth-first-search/bfs.js
+++ breadth-first-search/bfs.js
@@ -1,5 +1,10 @@
 var bfs = function(graph, startVertex, processVertex) {
     processVertex(startVertex);
+    
+    let current = graph.adjList[startVertex];
+    for(let vertex in current) {
+        processVertex(vertex);
+    }
 };
 
 module.exports = bfs;
